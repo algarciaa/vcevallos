8e45697d3459c66d6b365bca10a6fbcf9b671fcf
ceviviana.es <ceviviana.es@tecnologicoargos.edu.ec>
2025-11-07 00:19:49 +0000

    EXAMEN_SISTEMAS_VIVIANA

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from myapp!")
 
+#ingreso de cantidad 
+while True:
+    try:
+        cantidad = int(input("INGRESE CANTIDAD DE ALUMNOS: "))
+        if cantidad > 0:
+            break
+        else:
+            print("Error: La cantidad debe ser un numero entero positivo")
+    except ValueError:
+        print("Error: La cantidad debe ser un numero entero positivo")
 
-if __name__ == "__main__":
-    main()
